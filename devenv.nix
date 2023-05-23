5f4516cd6a4f048243682b9671f114786b15d8c0
Erik Karsten <erik@opcode.xyz>
2023-05-23 17:25:25 +0300

    add devenv

---
+++ devenv.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }:
+
+{
+  packages = with pkgs; [ ];
+  languages.python.enable = true;
+  languages.python.package = pkgs.python311.withPackages (ps: with ps; [ requests typer colorama rich shellingham ]);
+}
