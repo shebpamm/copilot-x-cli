ded4713bedcdf4d8374bbb58442fc08b0c6cb3db
Erik Karsten <erik@opcode.xyz>
2023-05-24 11:41:38 +0300

    get token from .config

--- auth.py
+++ auth.py
@@ -1,11 +1,28 @@
 """Get bearer token"""
-import requests
+import json
 import os
+
+import requests
 
 
 def get_copilot_token():
-    """Get copilot token from environment variable."""
-    return os.environ.get("COPILOT_TOKEN")
+    """
+    Get copilot token.
+
+    Firstly try getting it from COPILOT_TOKEN.
+    If this fails, try getting it from ~/.config/github-copilot/hosts.json
+    """
+    if "COPILOT_TOKEN" in os.environ:
+        return os.environ["COPILOT_TOKEN"]
+    else:
+        try:
+            with open(os.path.expanduser("~/.config/github-copilot/hosts.json")) as f:
+                data = json.load(f)
+                return data["github.com"]["oauth_token"]
+        except FileNotFoundError:
+            raise Exception(
+                "Please set COPILOT_TOKEN environment variable or install Github Copilot extension"
+            )
 
 
 def get_bearer():
