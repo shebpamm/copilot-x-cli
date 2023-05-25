f158ed6cdbdd09689428f3e0ec881b0c6696747c
Erik Karsten <erik@opcode.xyz>
2023-05-25 22:14:10 +0300

    add mildly cursed --base64 option for sending question as base64

--- x_cli/cli.py
+++ x_cli/cli.py
@@ -1,5 +1,6 @@
 """CLI interface for using copilot-x chat."""
 import typer
+from base64 import b64decode
 
 from . import api
 from .chat import ChatWindow
@@ -23,6 +24,7 @@ def chat():
 @app.command()
 def prompt(
     question: str = typer.Option(..., prompt=True),
+    base64: bool = False,
     shell: bool = False,
     chat: bool = False,
     code: bool = False,
@@ -36,6 +38,10 @@ def prompt(
         chat (bool): Whether to add chat instructions
         code (bool): Whether to add code instructions
     """
+
+    if base64:
+        question = b64decode(question).decode("utf-8")
+
     chatSession = api.ChatSession(shell=shell, chat=chat, code=code)
     response = chatSession.send_chat_blocking(question)
     print(response)
